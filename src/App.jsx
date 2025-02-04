3c98af99d1ee3733917010ed0a029c4ef779dd39
Ana <anawu.datawork@gmail.com>
2025-02-04 20:11:22 +0800

    update app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 import HomePage from "./pages/HomePage";
 import Map from "./pages/Map";
 import Signin from "./pages/Signin";
 import PageNotFound from "./pages/PageNotFound";
 
+const queryClient = new QueryClient();
+
 function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools />
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<HomePage />} />
@@ -16,7 +21,7 @@ function App() {
           <Route path="*" element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
-    </>
+    </QueryClientProvider>
   );
 }
 
