e612b56ceb1ca208be053e34a9446d21944826b9
Ana <anawu.datawork@gmail.com>
2025-02-12 17:01:53 +0800

    update

---
+++ src/components/form/SignupForm.jsx
@@ -0,0 +1,117 @@
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as z from "zod";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { PasswordInput } from "@/components/ui/password-input";
+
+const formSchema = z.object({
+  email: z.string(),
+  password: z.string(),
+});
+
+function SigninForm() {
+  const form = useForm({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  function onSubmit(values) {
+    try {
+      console.log(values);
+      // toast(
+      //   <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
+      //     <code className="text-white">{JSON.stringify(values, null, 2)}</code>
+      //   </pre>,
+      // );
+    } catch (error) {
+      console.error("Form submission error", error);
+      // toast.error("Failed to submit the form. Please try again.");
+    }
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+        <FormField
+          control={form.control}
+          name="username"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>會員名稱</FormLabel>
+              <FormControl>
+                <Input
+                  placeholder="請輸入會員名稱"
+                  type="text"
+                  {...field}
+                  className="bg-white focus-visible:border-none focus-visible:ring-main-500"
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>會員信箱</FormLabel>
+              <FormControl>
+                <Input
+                  placeholder="請輸入電子信箱"
+                  type="email"
+                  {...field}
+                  className="bg-white focus-visible:border-none focus-visible:ring-main-500"
+                />
+              </FormControl>
+              <FormDescription className="text-[12px]">
+                請輸入有效的電子信箱格式，例如：user@example.com
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>密碼</FormLabel>
+              <FormControl>
+                <PasswordInput
+                  placeholder="請輸入密碼"
+                  {...field}
+                  className="bg-white focus-visible:border-none focus-visible:ring-main-500"
+                />
+              </FormControl>
+              <FormDescription className="text-[12px]">
+                密碼長度至少為 8
+                個字元，建議使用包含大小寫字母、數字和符號的複雜密碼。
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <Button type="submit">註冊</Button>
+      </form>
+    </Form>
+  );
+}
+
+export default SigninForm;
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,4 +1,3 @@
-import { Button } from "@/components/ui/button";
 import {
   Card,
   CardContent,
@@ -7,66 +6,42 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
+
 import { Link } from "react-router-dom";
 
-import Footer from "@/components/Footer";
 import Header from "@/components/Header";
+import SignupForm from "@/components/form/SignupForm";
+import Footer from "@/components/Footer";
 
 const style = {
-  card: "mx-auto my-5 flex w-[800px] items-center justify-between overflow-hidden border-main-200 bg-main-100",
-  leftbox:
-    "bg-signin flex h-[500px] w-[500px] items-center bg-cover bg-center bg-no-repeat p-6",
-  rightbox: "h-[470px] w-[300px]",
+  cardContainer:
+    "mx-auto my-5 flex w-4/5 items-start justify-between overflow-hidden rounded-lg border-none bg-main-100 shadow shadow-neutral-400 lg:h-[600px]",
+  leftBox:
+    "hidden h-full items-center bg-signin bg-cover bg-center bg-no-repeat p-4 lg:flex lg:w-2/3",
 };
-
 function Signup() {
   return (
     <div>
       <Header />
-      <Card className={style.card}>
-        <div className={style.leftbox}>
-          <h3 className="text-white">
-            註冊帳號 <br />
-            加入返鄉大冒險
-          </h3>
-        </div>
-        <div className={style.rightbox}>
-          <CardHeader className="">
-            <CardTitle className="fs-3">註冊</CardTitle>
-            <CardDescription className="fs-4">
-              請輸入電子信箱及密碼
-            </CardDescription>
-          </CardHeader>
-          <CardContent>
-            <form>
-              <div className="grid w-full items-center gap-4">
-                <div className="flex flex-col space-y-1.5">
-                  <Label htmlFor="email">電子信箱</Label>
-                  <Input
-                    id="email"
-                    placeholder="請輸入電子信箱"
-                    className="bg-white"
-                  />
-                </div>
-                <div className="flex flex-col space-y-1.5">
-                  <Label htmlFor="password">密碼</Label>
-                  <Input
-                    id="password"
-                    placeholder="請輸入密碼"
-                    className="bg-white"
-                  />
-                </div>
-                <div className="flex flex-col space-y-1.5">
-                  <Label htmlFor="password">確認密碼</Label>
-                  <Input
-                    id="password"
-                    placeholder="再次輸入密碼"
-                    className="bg-white"
-                  />
-                </div>
-                <p className="text-center text-[12px]">
+      <main className="container mx-auto">
+        <Card className={style.cardContainer}>
+          <div className={style.leftBox}>
+            <p className="fs-1 font-bold text-white">
+              登入帳號 <br />
+              加入返鄉大冒險
+            </p>
+          </div>
+          <div className="w-full lg:w-1/3">
+            <CardHeader>
+              <CardTitle className="fs-3">註冊</CardTitle>
+              <CardDescription className="fs-5">
+                歡迎加入返箱村！
+              </CardDescription>
+            </CardHeader>
+            <CardContent>
+              <SignupForm />
+              <div className="mt-2">
+                <p className="fs-7 text-center">
                   註冊即表示您同意接受我們的 <br />
                   <Link className="text-blue-400 underline">
                     服務條款和隱私政策
@@ -77,17 +52,15 @@ function Signup() {
                   </Link>
                 </p>
               </div>
-            </form>
-          </CardContent>
-
-          <CardFooter className="flex flex-col justify-between">
-            <Button className="mb-3 ms-auto">註冊帳號</Button>
-            <Link to="/signin" className="fs-7 text-blue-400 underline">
-              已經有帳號了?
-            </Link>
-          </CardFooter>
-        </div>
-      </Card>
+            </CardContent>
+            <CardFooter className="justify-center">
+              <Link to="/signin" className="fs-7 text-blue-400 underline">
+                已經有帳號了?
+              </Link>
+            </CardFooter>
+          </div>
+        </Card>
+      </main>
       <Footer />
     </div>
   );
