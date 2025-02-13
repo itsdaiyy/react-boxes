01dedf5e7cf08b8e322c88e59c669bb5f5874dea
Ana <anawu.datawork@gmail.com>
2025-02-13 15:02:34 +0800

    update

--- src/services/apiBoxes.js
+++ src/services/apiBoxes.js
@@ -1,9 +1,9 @@
 import supabase from "./supabase";
 
 /**
- * 從 Supabase 取得所有紙箱資料
+ * 從 Supabase 取得紙箱狀態為可認領的紙箱資料
  *
- * 該函式向 Supabase 的 `boxes` 表格請求可認領紙箱列表的資料，並處理錯誤。
+ * 該函式向 Supabase 的 `boxes` 表格請求紙箱狀態為可認領的資料，並處理錯誤。
  *
  * @async
  * @function apiGetBoxesForSelling
@@ -11,6 +11,34 @@ import supabase from "./supabase";
  * @throws {Error} 如果請求過程中發生錯誤，則會拋出錯誤
  */
 export async function apiGetBoxesForSelling() {
+  try {
+    let { data: boxes, error } = await supabase
+      .from("boxes")
+      .select("*")
+      .in("status", ["可認領"]);
+
+    if (error) throw error;
+
+    return boxes;
+  } catch (error) {
+    // supabase 錯誤內容
+    console.error(error);
+    // UI 顯示的錯誤內容
+    throw new Error("無法取得 boxes 資料，請稍後再試");
+  }
+}
+
+/**
+ * 從 Supabase 取得 5-3 可認領紙箱列表的紙箱資料
+ *
+ * 該函式向 Supabase 的 `boxes` 表格請求 5-3 可認領紙箱列表的資料，並處理錯誤。
+ *
+ * @async
+ * @function apiGetBoxesForAdminManaging
+ * @returns {Promise<Array>} 紙箱資料陣列，若請求成功返回資料，若失敗則會拋出錯誤
+ * @throws {Error} 如果請求過程中發生錯誤，則會拋出錯誤
+ */
+export async function apiGetBoxesForAdminManaging() {
   try {
     let { data: boxes, error } = await supabase
       .from("boxes")
@@ -29,9 +57,9 @@ export async function apiGetBoxesForSelling() {
 }
 
 /**
- * 從 Supabase 取得所有紙箱資料
+ * 從 Supabase 取得 5-4 待回收紙箱列表的紙箱資料
  *
- * 該函式向 Supabase 的 `boxes` 表格請求待回收紙箱列表的資料，並處理錯誤。
+ * 該函式向 Supabase 的 `boxes` 表格請求 5-4 待回收紙箱列表的資料，並處理錯誤。
  *
  * @async
  * @function apiGetBoxesForScraping
