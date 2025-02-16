91df506e82b8c9b631931111ff499f32fb749ffd
oxfoxlion <oxfoxlion@gmail.com>
2025-02-17 00:37:05 +0800

    fix:樣式拆分，精簡化

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,6 @@
+import { NavLink } from "react-router-dom";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
-import { NavLink } from "react-router-dom";
 import homeSection2_1 from "@/assets/homeSection2-1.png";
 import homeSection2_2 from "@/assets/homeSection2-2.png";
 import homeSection2_3 from "@/assets/homeSection2-3.png";
@@ -14,9 +14,105 @@ import homeSection4_1 from "@/assets/homeSection4_1.png";
 import homeSection4_2 from "@/assets/homeSection4_2.png";
 import homeSection4_3 from "@/assets/homeSection4_3.png";
 
+// Section Card
+function Section2_Card({ imgUrl, title, content }) {
+  return (
+    <div className="flex flex-col items-center w-[360px]">
+      <img src={imgUrl} alt={title} className="mb-[24px]" />
+      <h3 className="mb-[16px]">{title}</h3>
+      <p>{content[0]}<br></br>{content[1]}</p>
+    </div>
+  )
+}
+
+function Section3_Card({ imgUrl, content, flexDirection }) {
+  return (
+    <div className={`flex py-[25px] px-[40px] items-center justify-center gap-[120px] ${flexDirection}`}>
+      <h4 className="text-second-300">{content}</h4>
+      <img src={imgUrl} alt={content} className="w-[615px] h-[350px]" />
+    </div>
+  )
+}
+
+function Section4_Card({ imgUrl, title, content }) {
+  return (
+    <div className="flex flex-col items-center justify-center bg-[url(@/assets/homeSection4_boxBg.png)] bg-center bg-contain bg-no-repeat p-[80px]">
+      <img src={imgUrl} alt={title} className="w-[160px] h-[160px]" />
+      <h3 className="my-[16px]">{title}</h3>
+      <p className="fs-6 text-center">{content[0]}<br></br>{content[1]}</p>
+    </div>
+  )
+}
+
+// Main 
 
 function HomePage() {
 
+  // Section Data
+  const section2Data = [
+    {
+      imgUrl: homeSection2_1,
+      title: '作為網購消費者',
+      content: ['太多網購紙箱囤積在家中', '丟棄浪費卻又沒有空間收藏']
+    },
+    {
+      imgUrl: homeSection2_2,
+      title: '做為小型店家',
+      content: ['每次出貨都需要採購', '許多一次性紙箱']
+    }
+  ]
+
+  const section3Data = [
+    {
+      imgUrl: homeSection3_1,
+      content: '1. 查詢附近紙箱轉運站，可回收紙箱類型',
+      flexDirection: 'flex-row'
+    },
+    {
+      imgUrl: homeSection3_2,
+      content: '2. 帶紙箱至轉運站回收',
+      flexDirection: 'flex-row-reverse'
+    },
+    {
+      imgUrl: homeSection3_3,
+      content: '3. 依據紙箱狀態，取得積分',
+      flexDirection: 'flex-row'
+    },
+    {
+      imgUrl: homeSection3_4,
+      content: '4. 查詢附近紙箱轉運站，待認領紙箱列表',
+      flexDirection: 'flex-row-reverse'
+    }, {
+      imgUrl: homeSection3_5,
+      content: '5. 至轉運站以積分兌換紙箱',
+      flexDirection: 'flex-row'
+    },
+    {
+      imgUrl: homeSection3_6,
+      content: '6. 現在你擁有更符合需求的紙箱',
+      flexDirection: 'flex-row-reverse'
+    }
+  ]
+
+  const section4Data = [
+    {
+      imgUrl: homeSection4_1,
+      title: '保留自用',
+      content: ['紙箱狀態尚可且符合您的需求', '可以選擇自行保留'],
+    },
+    {
+      imgUrl: homeSection4_2,
+      title: '釋出認領',
+      content: ['紙箱狀態尚可但無使用需求', '可以選擇釋出供民眾認領'],
+    },
+    {
+      imgUrl: homeSection4_3,
+      title: '進行回收',
+      content: ['紙箱狀態已無法供再次使用', '將紙箱交由本平台進行回收'],
+    }
+  ]
+
+  // style
   const bannerText = {
     textShadow: '0px 4px 4px rgba(0, 0, 0, 0.3), 0px 8px 12px rgba(0, 0, 0, 0.1)'
   }
@@ -41,16 +137,10 @@ function HomePage() {
         <div className="text-center py-[80px] relative z-10">
           <h2 className="h1 mb-[40px]">你也有這種困擾嗎？</h2>
           <div className="flex justify-center gap-[244px]">
-            <div className="flex flex-col items-center w-[360px]">
-              <img src={homeSection2_1} alt="網購消費者" className="mb-[24px]" />
-              <h3 className="mb-[16px]">作為網購消費者</h3>
-              <p>太多網購紙箱囤積在家中<br></br>丟棄浪費卻又沒有空間收藏</p>
-            </div>
-            <div className="flex flex-col items-center w-[360px]">
-              <img src={homeSection2_2} alt="小型店家" className="mb-[24px]" />
-              <h3 className="mb-[16px]">做為小型店家</h3>
-              <p>每次出貨都需要採購<br></br>許多一次性紙箱</p>
-            </div>
+            {section2Data.map((item, index) => (
+              <Section2_Card key={index} imgUrl={item.imgUrl} title={item.title} content={item.content}></Section2_Card>
+            ))}
+
           </div>
         </div>
         <img src={homeSection2_3} alt="有沒有更環保的選擇呢" className="relative z-10 w-full object-cover" />
@@ -63,35 +153,19 @@ function HomePage() {
         <h2 className="h1 mb-[80px]">加入返箱大冒險</h2>
         <div className="relative mb-[48px]">
           <h3 className="absolute top-0 left-0 text-second-300 bg-second-100 py-[16px] px-[40px] rounded-[50px]">將不用的紙箱回收</h3>
-          <div className="flex py-[25px] px-[40px] items-center justify-center gap-[120px] ">
-            <h4 className="text-second-300">1. 查詢附近紙箱轉運站，可回收紙箱類型</h4>
-            <img src={homeSection3_1} alt="查詢紙箱轉運站" className="w-[615px] h-[350px]" />
-          </div>
-          <div className="flex flex-row-reverse py-[25px] px-[40px] items-center justify-center gap-[120px]">
-            <h4 className="text-second-300">2. 帶紙箱至轉運站回收</h4>
-            <img src={homeSection3_2} alt="帶紙箱至轉運站回收" className="w-[615px] h-[350px]" />
-          </div>
-          <div className="flex py-[25px] px-[40px] items-center justify-center gap-[120px]">
-            <h4 className="text-second-300">3. 依據紙箱狀態，取得積分</h4>
-            <img src={homeSection3_3} alt="依據紙箱狀態取得積分" className="w-[615px] h-[350px]" />
-          </div>
+
+          {section3Data.slice(0, 3).map((item, index) => (
+            <Section3_Card key={index} imgUrl={item.imgUrl} content={item.content} flexDirection={item.flexDirection}></Section3_Card>
+          ))}
+
         </div>
         <div className="relative mb-[80px]">
           <h3 className="absolute top-0 right-0 text-main-600 bg-main-100 py-[16px] px-[40px] rounded-[50px] ">帶需要的紙箱回家</h3>
-          <div className="flex flex-row-reverse py-[25px] px-[40px] items-center justify-center gap-[120px]">
-            <h4 className="text-main-600">4. 查詢附近紙箱轉運站<br></br>待認領紙箱列表</h4>
-            <img src={homeSection3_4} alt="查詢附近站點待認領紙箱" className="w-[615px] h-[350px]" />
-          </div>
-          <div className="flex py-[25px] px-[40px] items-center justify-center gap-[120px]">
-            <h4 className="text-main-600">5. 至轉運站以積分兌換紙箱</h4>
-            <img src={homeSection3_5} alt="以積分兌換紙箱" className="w-[615px] h-[350px]" />
-          </div>
-          <div className="flex flex-row-reverse py-[25px] px-[40px] items-center justify-center gap-[120px]">
-            <h4 className="text-main-600">6. 現在你擁有更符合需求的紙箱</h4>
-            <img src={homeSection3_6} alt="現在你擁有更符合需求的紙箱" className="w-[615px] h-[350px]" />
-          </div>
+          {section3Data.slice(3, 6).map((item, index) => (
+            <Section3_Card key={index} imgUrl={item.imgUrl} content={item.content} flexDirection={item.flexDirection}></Section3_Card>
+          ))}
         </div>
-        <NavLink to="/signin" className="btn z-10 inline-block">馬上註冊<br></br>加入返箱大冒險</NavLink>
+        <NavLink to="/signup" className="btn z-10 inline-block">馬上註冊<br></br>加入返箱大冒險</NavLink>
       </div>
       {/* 區塊四 */}
       <div className="bg-[url(@/assets/homeSection4_background.png)] bg-center bg-cover bg-no-repeat py-[160px]">
@@ -101,21 +175,12 @@ function HomePage() {
             <p className="text-[#6F6F6F] fs-3">收到紙箱後，您可以決定紙箱的去向<br></br>成為為紙箱的領路人</p>
           </div>
           <div className="flex mx-auto justify-center items-center gap-[24px] mb-[80px]">
-            <div className="flex flex-col items-center justify-center bg-[url(@/assets/homeSection4_boxBg.png)] bg-center bg-contain bg-no-repeat p-[80px]">
-              <img src={homeSection4_1} alt="保留自用" className="w-[160px] h-[160px]"/>
-              <h3 className="my-[16px]">保留自用</h3>
-              <p className="fs-6 text-center">紙箱狀態尚可且符合您的需求<br></br>可以選擇自行保留</p>
-            </div>
-            <div className="flex flex-col items-center justify-center bg-[url(@/assets/homeSection4_boxBg.png)] bg-center bg-contain bg-no-repeat p-[80px]">
-              <img src={homeSection4_2} alt="釋出認領" className="w-[160px] h-[160px]"/>
-              <h3 className="my-[16px]">釋出認領</h3>
-              <p className="fs-6 text-center">紙箱狀態尚可但無使用需求<br></br>可以選擇釋出供民眾認領</p>
-            </div>
-            <div className="flex flex-col items-center justify-center bg-[url(@/assets/homeSection4_boxBg.png)] bg-center bg-contain bg-no-repeat p-[80px]">
-              <img src={homeSection4_3} alt="進行回收" className="w-[160px] h-[160px]"/>
-              <h3 className="my-[16px]">進行回收</h3>
-              <p className="fs-6 text-center">紙箱狀態已無法供再次使用<br></br>將紙箱交由本平台進行回收</p>
-            </div>
+            {
+              section4Data.map((item,index) => (
+                <Section4_Card key={index} imgUrl={item.imgUrl} title={item.title} content={item.content}></Section4_Card>
+              ))
+            }
+
           </div>
 
           <NavLink to="/signin" className="btn z-10">申請成為紙箱轉運站</NavLink>
