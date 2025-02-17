0836d74f31f4721e548a867f21cd88bd02e84ac5
Ana <anawu.datawork@gmail.com>
2025-02-17 21:12:09 +0800

    update apiGetBoxes.js and useBoxes.js

--- src/hooks/useBoxes.js
+++ src/hooks/useBoxes.js
@@ -1,12 +1,13 @@
 import {
   apiGetBoxesForSelling,
+  apiGetBoxesForAdminManaging,
   apiGetBoxesForScraping,
 } from "@/services/apiBoxes";
 import { useQuery } from "@tanstack/react-query";
 /**
- * 自訂 Hook：使用 React Query 來取得紙箱資料
+ * 自訂 Hook：使用 React Query 來取得紙箱狀態為可認領的紙箱資料
  *
- * 使用 `useQuery` 來向 API 請求紙箱資料，並處理資料加載與錯誤狀態。
+ * 使用 `useQuery` 來向 API 請求紙箱狀態為可認領的紙箱資料，並處理資料加載與錯誤狀態。
  *
  * @returns {Object} 返回包含三個屬性的物件：
  *   - `boxes` {Array|null} - 紙箱資料陣列，若尚未請求或發生錯誤則為 `null`
@@ -25,11 +26,33 @@ export function useBoxesForSelling() {
 
   return { boxes, isLoadingBoxes, boxesError };
 }
+/**
+ * 自訂 Hook：使用 React Query 來取得 5-3 可認領紙箱列表的紙箱資料
+ *
+ * 使用 `useQuery` 來向 API 請求可認領紙箱列表的紙箱資料，並處理資料加載與錯誤狀態。
+ *
+ * @returns {Object} 返回包含三個屬性的物件：
+ *   - `boxes` {Array|null} - 紙箱資料陣列，若尚未請求或發生錯誤則為 `null`
+ *   - `isLoadingBoxes` {boolean} - 是否正在加載資料
+ *   - `BoxesError` {Error|null} - 若請求發生錯誤，將包含錯誤物件，否則為 `null`
+ */
+export function useBoxesForAdminManaging() {
+  const {
+    data: boxes,
+    isLoading: isLoadingBoxes,
+    error: boxesError,
+  } = useQuery({
+    queryKey: ["boxes", "managing"],
+    queryFn: apiGetBoxesForAdminManaging,
+  });
+
+  return { boxes, isLoadingBoxes, boxesError };
+}
 
 /**
- * 自訂 Hook：使用 React Query 來取得紙箱資料
+ * 自訂 Hook：使用 React Query 來取得 5-4 待回收紙箱列表的紙箱資料
  *
- * 使用 `useQuery` 來向 API 請求紙箱資料，並處理資料加載與錯誤狀態。
+ * 使用 `useQuery` 來向 API 請求 5-4 待回收紙箱列表的紙箱資料，並處理資料加載與錯誤狀態。
  *
  * @returns {Object} 返回包含三個屬性的物件：
  *   - `boxes` {Array|null} - 紙箱資料陣列，若尚未請求或發生錯誤則為 `null`
