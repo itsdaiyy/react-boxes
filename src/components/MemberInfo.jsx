64255ff15b43cb0a3fa6f5d8586e4b87b47ba40b
johnny <johnnyhsieh@gmail.com>
2025-03-02 15:51:25 +0800

    update

--- src/components/MemberInfo.jsx
+++ src/components/MemberInfo.jsx
@@ -1,11 +1,14 @@
 import village_master from "../assets/village_master.svg";
-import points from "../assets/points.svg";
+import points_icon from "../assets/points.svg";
 import box_count from "../assets/box_count.svg";
 
 import ResponsiveSwiper from "./ui/ResponsiveSwiper";
 import MemberInfoForm from "./form/MemberInfoForm";
 
 import { useMember } from "@/hooks/useMember";
+import { useEffect, useState } from "react";
+import Spinner from "./Spinner";
+import ErrorMessage from "./ErrorMessage";
 
 const style = {
   cardContainer: "flex items-center justify-around rounded-2xl bg-white p-10",
@@ -15,7 +18,57 @@ const style = {
 
 function MemberInfo() {
   const { member, isLoadingMember, getMemberError } = useMember();
-  console.log(member);
+  const [pointNum, setPointNum] = useState("");
+  const [transactionNums, setTransactionNums] = useState("");
+
+  useEffect(() => {
+    if (member && member.user_metadata) {
+      // setData(member);
+      // console.log(data);
+      setPointNum(member.user_metadata.points);
+      setTransactionNums(member.user_metadata.transaction_nums);
+    }
+  }, [member]);
+
+  // 會員等級定義 => 轉運紙箱數為門檻
+  // transactionNums > 0 => 相遇路人
+  // transactionNums > 50 => 返箱青年
+  // transactionNums > 100 => 箱村村長
+  // transactionNums > 200 => 箱村守護者
+  const memberTitle = {
+    level_1: "相遇路人",
+    level_2: "返箱青年",
+    level_3: "箱村村長",
+    level_4: "箱村守護者",
+  };
+  const [memberLevel, setMemberLevel] = useState(1);
+  const [memberLevelTitle, setMemberLevelTitle] = useState("");
+  const [levelUpNum, setLevelUpNum] = useState("");
+  useEffect(() => {
+    if (transactionNums > 0 && transactionNums < 50) {
+      setMemberLevel(1);
+      setMemberLevelTitle(memberTitle.level_2);
+      setLevelUpNum(50 - transactionNums);
+    } else if (transactionNums >= 50 && transactionNums < 100) {
+      setMemberLevel(2);
+      setMemberLevelTitle(memberTitle.level_3);
+      setLevelUpNum(100 - transactionNums);
+    } else if (transactionNums >= 100 && transactionNums < 200) {
+      setMemberLevel(3);
+      setMemberLevelTitle(memberTitle.level_4);
+      setLevelUpNum(200 - transactionNums);
+    } else if (transactionNums > 200) {
+      setMemberLevel(4);
+    }
+  }, [
+    memberTitle.level_2,
+    memberTitle.level_3,
+    memberTitle.level_4,
+    transactionNums,
+  ]);
+
+  if (isLoadingMember) return <Spinner />;
+  if (getMemberError) return <ErrorMessage errorMessage={member.message} />;
 
   return (
     <div className="w-full text-center">
@@ -24,18 +77,13 @@ function MemberInfo() {
           <p className="mb-6 text-base md:text-2xl">
             幫助紙箱君順利搭上返箱專車，繼續旅行吧！
           </p>
-          <div className="md:flex md:items-center md:justify-center">
-            <p className="my-5 flex items-center justify-center text-base md:text-2xl">
-              再累積
-              <span className="mx-5 flex items-center text-[40px] font-bold text-main-500">
-                486
-              </span>
-              次
-            </p>
-            <p className="my-5 flex items-center justify-center text-base md:text-2xl">
-              轉運紙箱數，就可以解鎖箱村守護者稱號！
-            </p>
-          </div>
+
+          {memberLevel !== 4 && (
+            <NormalLevel
+              levelUpNum={levelUpNum}
+              memberLevelTitle={memberLevelTitle}
+            />
+          )}
         </div>
         <ResponsiveSwiper />
       </div>
@@ -54,15 +102,19 @@ function MemberInfo() {
             {/* 2號 div - 手機版時在最下方 */}
             <div className="hidden gap-5 rounded md:flex md:flex-col">
               <div className={style.cardContainer}>
-                <img src={points} alt="" />
+                <img src={points_icon} alt="" />
                 <p className={style.cardText}>當前積分</p>
-                <p className={style.cardNumber}>35</p>
+                <p className={style.cardNumber}>
+                  {pointNum !== "" ? pointNum : "載入中..."}
+                </p>
                 <p className={style.cardText}>Points</p>
               </div>
               <div className={style.cardContainer}>
                 <img src={box_count} alt="" />
                 <p className={style.cardText}>轉運紙箱數</p>
-                <p className={style.cardNumber}>514</p>
+                <p className={style.cardNumber}>
+                  {transactionNums !== "" ? transactionNums : "載入中..."}
+                </p>
                 <p className={style.cardText}>次</p>
               </div>
             </div>
@@ -70,21 +122,25 @@ function MemberInfo() {
 
           {/* 會員資訊表單 */}
           {/* 3號 div - 左側容器，手機版時在中間 */}
-          <MemberInfoForm />
+          <MemberInfoForm data={member} />
 
           {/* 會員資訊 */}
           {/* 2號 div 的手機版位置 */}
           <div className="order-3 flex flex-col gap-5 rounded md:hidden">
             <div className={`${style.cardContainer} flex-col gap-4`}>
-              <img src={points} alt="" />
+              <img src={points_icon} alt="" />
               <p className={style.cardText}>當前積分</p>
-              <p className={style.cardNumber}>35</p>
+              <p className={style.cardNumber}>
+                {pointNum !== "" ? pointNum : "載入中..."}
+              </p>
               <p className={style.cardText}>Points</p>
             </div>
             <div className={`${style.cardContainer} flex-col gap-4`}>
               <img src={box_count} alt="" />
               <p className={style.cardText}>轉運紙箱數</p>
-              <p className={style.cardNumber}>514</p>
+              <p className={style.cardNumber}>
+                {transactionNums !== "" ? transactionNums : "載入中..."}
+              </p>
               <p className={style.cardText}>次</p>
             </div>
           </div>
@@ -95,3 +151,23 @@ function MemberInfo() {
 }
 
 export default MemberInfo;
+
+// 一般等級
+function NormalLevel({ levelUpNum, memberLevelTitle }) {
+  return (
+    <div className="md:flex md:items-center md:justify-center">
+      <p className="my-5 flex items-center justify-center text-base md:text-2xl">
+        再累積
+        <span className="mx-5 flex items-center text-[40px] font-bold text-main-500">
+          {levelUpNum}
+        </span>
+        次
+      </p>
+      <p className="my-5 flex items-center justify-center text-base md:text-2xl">
+        轉運紙箱數，就可以解鎖
+        <span className="px-1 font-bold text-main-500">{memberLevelTitle}</span>
+        稱號！
+      </p>
+    </div>
+  );
+}
--- src/components/form/MemberInfoForm.jsx
+++ src/components/form/MemberInfoForm.jsx
@@ -16,6 +16,7 @@ import { Input } from "@/components/ui/input";
 
 // Icon
 import { FaPen } from "react-icons/fa";
+import { MdClose } from "react-icons/md";
 import { useUpdateMember } from "@/hooks/useUpdateMember";
 
 // zod驗證規則
@@ -26,22 +27,27 @@ const formSchema = z.object({
   email: z.string().email("請輸入有效的電子郵件"),
 });
 
-function MemberInfoForm() {
+function MemberInfoForm(data) {
   const [isEditing, setIsEditing] = useState(false);
+
+  const { updateMember, updateMemberError, isUpdating } = useUpdateMember();
+  // console.log(data);
+
+  // const [name, setName] = useState(data.user_metadata)
+
+  // useEffect(() =>{
+
+  // },[data])
 
   const form = useForm({
     resolver: zodResolver(formSchema),
     defaultValues: {
       name: "",
-      username: "",
       phone: "",
-      email: "",
     },
   });
   const { reset, formState } = form;
   const { isSubmitSuccessful } = formState;
-
-  const { updateMember, updateMemberError, isUpdating } = useUpdateMember();
 
   // update 物件格式：
   // const obj = {
@@ -57,16 +63,21 @@ function MemberInfoForm() {
   // 串接API後繼續完成
   const onSubmit = (data) => {
     console.log(data);
-    // setIsEditing(false);
+    console.log("上傳的圖片檔案:", data.avatar);
+    setIsEditing(false);
   };
 
   return (
     <div className="order-2 rounded-3xl bg-white p-10 md:order-1 md:w-1/2">
       <div className="mb-6 flex items-center justify-between">
         <p className="text-2xl font-bold text-gray-700">箱村村長</p>
-        <div className="flex items-center">
+        <div className="flex items-center justify-center">
           <button type="button" onClick={() => setIsEditing(!isEditing)}>
-            <FaPen className="text-main-600" size={20} />
+            {isEditing ? (
+              <MdClose className="text-main-600" size={30} />
+            ) : (
+              <FaPen className="text-main-600" size={20} />
+            )}
           </button>
         </div>
       </div>
@@ -94,29 +105,6 @@ function MemberInfoForm() {
               </FormItem>
             )}
           />
-
-          <FormField
-            control={form.control}
-            name="username"
-            render={({ field }) => (
-              <FormItem className="mb-6">
-                <FormLabel className="block text-start text-gray-700">
-                  會員暱稱
-                </FormLabel>
-                <FormControl>
-                  <Input
-                    placeholder="請輸入會員名稱"
-                    type="text"
-                    {...field}
-                    className="bg-white py-4 focus-visible:border-none focus-visible:ring-main-500"
-                    disabled={!isEditing}
-                  />
-                </FormControl>
-                <FormMessage className="block text-start" />
-              </FormItem>
-            )}
-          />
-
           <FormField
             control={form.control}
             name="phone"
@@ -138,20 +126,24 @@ function MemberInfoForm() {
               </FormItem>
             )}
           />
-
           <FormField
             control={form.control}
-            name="email"
-            render={({ field }) => (
+            name="avatar"
+            render={({ field: { onChange, ...fieldProps } }) => (
               <FormItem className="mb-6">
                 <FormLabel className="block text-start text-gray-700">
-                  電子信箱
+                  上傳頭貼
                 </FormLabel>
                 <FormControl>
                   <Input
-                    placeholder="電子信箱"
-                    type="email"
-                    {...field}
+                    type="file"
+                    accept="image/*"
+                    onChange={(e) => {
+                      const file = e.target.files?.[0]; // 取得選擇的檔案
+                      if (file) {
+                        onChange(file); // 更新表單的值
+                      }
+                    }}
                     className="bg-white py-4 focus-visible:border-none focus-visible:ring-main-500"
                     disabled={!isEditing}
                   />
