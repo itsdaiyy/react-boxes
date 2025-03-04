692c9f388252af899afa0b3983713186c4ad03a1
johnny <johnnyhsieh@gmail.com>
2025-03-04 18:44:18 +0800

    feat: 4-4串接API完成

--- src/components/table/MemberInfoHistoryTable.jsx
+++ src/components/table/MemberInfoHistoryTable.jsx
@@ -2,53 +2,55 @@
 import DataTable from "react-data-table-component";
 import { customStyles, paginationComponentOptions } from "@/data/constants";
 import { useMemberTransactionRecords } from "@/hooks/useBoxTransactions";
-
-// 串接資料
+import Spinner from "../Spinner";
+import ErrorMessage from "../ErrorMessage";
 
 const MemberInfoHistoryTable = () => {
   // 欄位
   const columns = [
     {
       name: "交易時間",
-      selector: (row) => row.time,
+      selector: (row) => row.created_at.replace("T", " ").slice(0, 16),
     },
     {
       name: "交易形式",
-      selector: (row) => row.content,
+      selector: (row) => row.transaction_type,
     },
     {
       name: "交易站點",
-      selector: (row) => row.site,
+      selector: (row) => row.station_name_snapshot,
     },
     {
       name: "紙箱大小",
-      selector: (row) => row.site,
+      selector: (row) => row.size,
     },
     {
       name: "紙箱保存等級",
-      selector: (row) => row.site,
+      selector: (row) => row.condition,
     },
     {
       name: "換算積分",
-      selector: (row) => row.pointChange,
-      cell: (row) => (
-        <span
-          style={{
-            color: row.pointChange.startsWith("+") ? "green" : "red",
-          }}
-        >
-          {row.pointChange}
-        </span>
-      ),
+      selector: (row) => {
+        if (
+          row.transaction_type === "回收" ||
+          row.transaction_type === "購買"
+        ) {
+          return "+" + row.earned_points;
+        } else {
+          return "-" + row.points_cost;
+        }
+      },
     },
   ];
   const { records, isLoadingRecords, recordsError } =
     useMemberTransactionRecords("8b9acdef-b856-4c78-ac16-36d199737957");
   console.log(records);
+  if (isLoadingRecords) return <Spinner />;
+  if (recordsError) return <ErrorMessage errorMessage={recordsError.message} />;
   return (
     <DataTable
       columns={columns}
-      // data={member}
+      data={records}
       pagination
       customStyles={customStyles}
       paginationComponentOptions={paginationComponentOptions}
