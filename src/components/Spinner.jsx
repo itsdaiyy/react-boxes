e1fa6bd5dab02279033a6abafb5a56ea37f5427d
ollie <behoya11@gmail.com>
2025-04-19 15:32:31 +0800

    style: 移除不必要 border

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -1,6 +1,6 @@
 export default function Spinner() {
   return (
-    <div className="flex h-screen items-center justify-center border">
+    <div className="flex h-screen items-center justify-center">
       <div className="h-8 w-8 animate-spin rounded-full border-4 border-main-100 border-t-main-600"></div>
     </div>
   );
