71ba5c0b686c643645713bc3619eb450fdf0ae43
ollie <behoya11@gmail.com>
2025-05-01 12:52:48 +0800

    fix: 新註冊一般會員，會員資訊頁面基本資訊顯示問題
    問題描述：
      1. 新註冊的一般會員，會員資訊頁上方的資訊欄位沒有顯示。
    
    主要變更：
        1. NormalDashboard.jsx 調用 BannerInfo 組件，調整傳入 infoData 的資料格式，
           補上 label、value、type 屬性。
        2. BannerInfo 組件，調整欄位為空值時，有提示訊息「（未提供，請於會員頁面更新${label}）」
    
    異動檔案：
    	modified:   src/features/admin/BannerInfo.jsx
    	modified:   src/features/member/NormalDashboard.jsx

--- src/features/member/NormalDashboard.jsx
+++ src/features/member/NormalDashboard.jsx
@@ -13,9 +13,13 @@ function NormalDashboard({ member }) {
         <BannerInfo
           title={member.user.user_metadata.display_name}
           infoData={[
-            `會員編號：${member.user.id}`,
-            `電子信箱：${member.user.email}`,
-            `聯絡電話：${member.user.user_metadata.phone}`,
+            { label: "會員編號", value: member.user.id },
+            { label: "電子信箱", value: member.user.email, type: "email" },
+            {
+              label: "聯絡電話",
+              value: member.user.user_metadata.phone,
+              type: "tel",
+            },
           ]}
           showApplyButton={true}
         />
